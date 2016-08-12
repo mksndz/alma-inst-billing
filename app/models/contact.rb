350ed92528434c565c401c4a7f9ce8e04524ccf3
Mike Kanning <mikekanning@gmail.com>
2016-08-12 14:47:59 -0400

    contacts minimal validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,11 @@
 class Contact < ActiveRecord::Base
 
+  validates_presence_of :name
+  validates_presence_of :email
+
+  validates_uniqueness_of :name
+  validates_uniqueness_of :email
+
   belongs_to :institution
 
 end
