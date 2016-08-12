489709c9376e258704bbdc5a6718ceb9f83531d5
Mike Kanning <mikekanning@gmail.com>
2016-08-12 18:27:42 -0400

    more validations and model specs for BookPrice

--- spec/models/book_price_spec.rb
+++ spec/models/book_price_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe BookPrice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'has a String lc_start' do
+    bp = Fabricate :book_price
+    expect(bp.lc_start).to be_a String
+  end
+
+  it 'has a String lc_end' do
+    bp = Fabricate :book_price
+    expect(bp.lc_end).to be_a String
+  end
+
+  it 'has a String description' do
+    bp = Fabricate :book_price
+    expect(bp.description).to be_a String
+  end
+
+  it 'has a Integer price' do
+    bp = Fabricate :book_price
+    expect(bp.price).to be_a Integer
+  end
+
+  # VALIDATIONS
+
+  it 'must have a unique lc_start' do
+    Fabricate :book_price
+    expect {
+      Fabricate :book_price
+    }.to raise_error ActiveRecord::RecordInvalid
+  end
+
+
+  it 'must have a unique lc_end' do
+    Fabricate :book_price
+    expect {
+      Fabricate :book_price
+    }.to raise_error ActiveRecord::RecordInvalid
+  end
+
+  it 'must have a unique description' do
+    Fabricate :book_price
+    expect {
+      Fabricate :book_price
+    }.to raise_error ActiveRecord::RecordInvalid
+  end
+
 end
