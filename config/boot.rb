25e4d338d8b8578d9ace1b793a0c62d72078cd15
Mike Kanning <mikekanning@gmail.com>
2016-08-12 17:56:25 -0400

    add note about server configuration override  to support Vagrant

--- config/boot.rb
+++ config/boot.rb
@@ -3,6 +3,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'rails/commands/server'
 
+# supports running development server in Vagrant instance
 module Rails
   class Server
     alias :default_options_bk :default_options
