b8c1d02e33ca2492ee037dbc9ac8c1ce5e82deed
Mike Kanning <mikekanning@gmail.com>
2016-08-17 13:15:53 -0400

    make api key a required value

--- app/models/institution.rb
+++ app/models/institution.rb
@@ -1,6 +1,6 @@
 class Institution < ActiveRecord::Base
 
-  validates_presence_of :name
+  validates_presence_of :name, :api_key
   validates_uniqueness_of :name
 
   has_many :contacts, dependent: :destroy
