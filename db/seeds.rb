bfc241c9da3e06f6687a8e1206182903e2efd50c
Mike Kanning <mikekanning@gmail.com>
2016-08-17 09:49:57 -0400

    seed institution names and current book price values

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,365 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Load Institutions and Book Price values
+
+institutions = [
+    {
+        name: 'Abraham Baldwin Agricultural College'
+    },
+    {
+        name: 'Albany State University'
+    },
+    {
+        name: 'Armstrong State University'
+    },
+    {
+        name: 'Atlanta Metropolitan College'
+    },
+    {
+        name: 'Augusta University'
+    },
+    {
+        name: 'Bainbridge State College'
+    },
+    {
+        name: 'Clayton State University'
+    },
+    {
+        name: 'College of Coastal Georgia'
+    },
+    {
+        name: 'Columbus State University'
+    },
+    {
+        name: 'Dalton State College'
+    },
+    {
+        name: 'Darton State College'
+    },
+    {
+        name: 'East Georgia State College'
+    },
+    {
+        name: 'Fort Valley State University'
+    },
+    {
+        name: 'Georgia College & State University'
+    },
+    {
+        name: 'Georgia Gwinnett College'
+    },
+    {
+        name: 'Georgia Highlands College'
+    },
+    {
+        name: 'Georgia Piedmont Technical College'
+    },
+    {
+        name: 'Georgia Southern University'
+    },
+    {
+        name: 'Georgia Southwest State University'
+    },
+    {
+        name: 'Georgia State University'
+    },
+    {
+        name: 'Gordon State College'
+    },
+    {
+        name: 'Kennesaw State University'
+    },
+    {
+        name: 'Middle Georgia State College'
+    },
+    {
+        name: 'Savannah State University'
+    },
+    {
+        name: 'South Georgia State College'
+    },
+    {
+        name: 'University of Georgia'
+    },
+    {
+        name: 'University of North Georgia'
+    },
+    {
+        name: 'Valdosta State University'
+    },
+    {
+        name: 'University of West Georgia'
+    },
+    {
+        name: 'University System'
+    },
+
+]
+
+book_prices = [
+    {
+        lc_start: 'A',
+        lc_end: 'AZ',
+        description: 'General Works',
+        price: 110.81
+    },
+    {
+        lc_start: 'B',
+        lc_end: 'BE',
+        description: 'Philosophy & Religion',
+        price: 97.28
+    },
+    {
+        lc_start: 'BF',
+        lc_end: 'BF',
+        description: 'Psychology',
+        price: 115.22
+    },
+    {
+        lc_start: 'BG',
+        lc_end: 'BZ',
+        description: 'Philosophy & Religion',
+        price: 97.28
+    },
+    {
+        lc_start: 'C',
+        lc_end: 'CZ',
+        description: 'Auxiliary Sciences of History',
+        price: 83.35
+    },
+    {
+        lc_start: 'D',
+        lc_end: 'DZ',
+        description: 'History: General and Old World',
+        price: 83.35
+    },
+    {
+        lc_start: 'E',
+        lc_end: 'EZ',
+        description: 'History: America - General',
+        price: 83.35
+    },
+    {
+        lc_start: 'F',
+        lc_end: 'FZ',
+        description: 'History: United States',
+        price: 83.35
+    },
+    {
+        lc_start: 'G',
+        lc_end: 'GM',
+        description: 'Geography',
+        price: 120.75
+    },
+    {
+        lc_start: 'GN',
+        lc_end: 'GN',
+        description: 'Anthropology',
+        price: 101.62
+    },
+    {
+        lc_start: 'GO',
+        lc_end: 'GU',
+        description: 'Geography',
+        price: 120.75
+    },
+    {
+        lc_start: 'GV',
+        lc_end: 'GV',
+        description: 'Physical Education & Resources',
+        price: 120.75
+    },
+    {
+        lc_start: 'GW',
+        lc_end: 'GZ',
+        description: 'Geography',
+        price: 120.75
+    },
+    {
+        lc_start: 'H',
+        lc_end: 'HL',
+        description: 'Business & Economics',
+        price: 117.50
+    },
+    {
+        lc_start: 'HM',
+        lc_end: 'HM',
+        description: 'Sociology',
+        price: 114.34
+    },
+    {
+        lc_start: 'HN',
+        lc_end: 'HZ',
+        description: 'Business & Economics',
+        price: 117.50
+    },
+    {
+        lc_start: 'I',
+        lc_end: 'IZ',
+        description: 'General',
+        price: 57.09
+    },
+    {
+        lc_start: 'J',
+        lc_end: 'JZ',
+        description: 'Political Science',
+        price: 106.53
+    },
+    {
+        lc_start: 'K',
+        lc_end: 'KZ',
+        description: 'Law',
+        price: 161.29
+    },
+    {
+        lc_start: 'L',
+        lc_end: 'LZ',
+        description: 'Education',
+        price: 96.65
+    },
+    {
+        lc_start: 'M',
+        lc_end: 'NZ',
+        description: 'Fine & Applied Arts',
+        price: 69.08
+    },
+    {
+        lc_start: 'O',
+        lc_end: 'OZ',
+        description: 'General',
+        price: 57.09
+    },
+    {
+        lc_start: 'P',
+        lc_end: 'PZ',
+        description: 'Literature & Language',
+        price: 69.50
+    },
+    {
+        lc_start: 'Q',
+        lc_end: 'Q',
+        description: 'Science (General)',
+        price: 128.18
+    },
+    {
+        lc_start: 'QA',
+        lc_end: 'QA',
+        description: 'Mathematics & Computer Science',
+        price: 133.96
+    },
+    {
+        lc_start: 'QB',
+        lc_end: 'QC',
+        description: 'Physics & Astronomy',
+        price: 149.09
+    },
+    {
+        lc_start: 'QD',
+        lc_end: 'QD',
+        description: 'Chemistry',
+        price: 238.81
+    },
+    {
+        lc_start: 'QE',
+        lc_end: 'QE',
+        description: 'Geology',
+        price: 238.81
+    },
+    {
+        lc_start: 'QF',
+        lc_end: 'QG',
+        description: 'Science (General)',
+        price: 128.18
+    },
+    {
+        lc_start: 'QH',
+        lc_end: 'QH',
+        description: 'Natural History (Biology)',
+        price: 161.11
+    },
+    {
+        lc_start: 'QI',
+        lc_end: 'QJ',
+        description: 'Science (General)',
+        price: 128.18
+    },
+    {
+        lc_start: 'QK',
+        lc_end: 'QK',
+        description: 'Botany',
+        price: 156.65
+    },
+    {
+        lc_start: 'QL',
+        lc_end: 'QR',
+        description: 'Zoology - Microbiology',
+        price: 161.11
+    },
+    {
+        lc_start: 'QS',
+        lc_end: 'QZ',
+        description: 'Science (General)',
+        price: 161.11
+    },
+    {
+        lc_start: 'R',
+        lc_end: 'RZ',
+        description: 'Medicine',
+        price: 128.67
+    },
+    {
+        lc_start: 'S',
+        lc_end: 'SZ',
+        description: 'Agriculture',
+        price: 99.44
+    },
+    {
+        lc_start: 'T',
+        lc_end: 'TS',
+        description: 'Engineering & Technology',
+        price: 157.88
+    },
+    {
+        lc_start: 'TT',
+        lc_end: 'TT',
+        description: 'Industrial Arts',
+        price: 43.71
+    },
+    {
+        lc_start: 'TU',
+        lc_end: 'TW',
+        description: 'Engineering & Technology',
+        price: 157.88
+    },
+    {
+        lc_start: 'TX',
+        lc_end: 'TX',
+        description: 'Home Economics',
+        price: 67.40
+    },
+    {
+        lc_start: 'TY',
+        lc_end: 'TZ',
+        description: 'Engineering & Technology',
+        price: 157.88
+    },
+    {
+        lc_start: 'U',
+        lc_end: 'VZ',
+        description: 'Military & Naval Science',
+        price: 81.76
+    },
+    {
+        lc_start: 'W',
+        lc_end: 'YZ',
+        description: 'General',
+        price: 75.60
+    },
+    {
+        lc_start: 'Z',
+        lc_end: 'ZZ',
+        description: 'Library & Information Science',
+        price: 108.53
+    },
+]
+
+Institution.create  institutions
+BookPrice.create    book_prices
