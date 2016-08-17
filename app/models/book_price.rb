47d8b1c769769c3ad2eae5ce1a6cab6e92aa5fac
Mike Kanning <mikekanning@gmail.com>
2016-08-17 08:05:20 -0400

    remove uniqueness requirement for BookPrice description

--- app/models/book_price.rb
+++ app/models/book_price.rb
@@ -1,6 +1,6 @@
 class BookPrice < ActiveRecord::Base
 
   validates_presence_of :lc_start, :lc_end, :description, :price
-  validates_uniqueness_of :lc_start, :lc_end, :description
+  validates_uniqueness_of :lc_start, :lc_end
 
 end
