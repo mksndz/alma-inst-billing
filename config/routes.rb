fbe51b8c7a47cb29916d16b06e8d55793e445ac5
Mike Kanning <mikekanning@gmail.com>
2016-08-17 14:56:44 -0400

    rudimentary institution sandbox area for testing api functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   resources :book_prices, except: [:show]
   resources :institutions do
     resources :contacts
+
+    get 'sandbox', to: 'alma#index', as: :alma_api_sandbox
+    get 'users', to: 'alma#get_users', as: :users
+    get 'user/:user_id', to: 'alma#get_user', as: :user
+    get 'user/:user_id/fees', to: 'alma#get_user_fees', as: :user_fees
+    get 'user/:user_id/fee/:fee_id', to: 'alma#get_user_fee', as: :user_fee
+
   end
 
   root 'application#index'
